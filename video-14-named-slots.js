7ee9d159d51e708f654489c08b5cca36da1713d2
jeremyclayton <jeremy.clayton.91@gmail.com>
2017-03-08 16:09:16 -0700

    finsihed video 14

--- video-14-named-slots.js
+++ video-14-named-slots.js
@@ -1,26 +1,36 @@
-window.Event = new Vue();
 
 
-Vue.component('coupon', {
-    template: `<input placeholder="Enter your coupon code" @blur="onCouponApplied">`,
 
-    methods: {
-        onCouponApplied(){
-            Event.$emit('applied');
-        }
-    }
+Vue.component('modal', {
+    template: `
+    <div class="modal is-active">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">
+
+        <slot name="header"></slot>
+
+      </p>
+      <button class="delete"></button>
+    </header>
+    <section class="modal-card-body">
+      <slot>
+
+      </slot>
+    </section>
+    <footer class="modal-card-foot">
+        <slot name="footer">
+      <a class="button is-success">Okay</a>
+      </slot>
+    </footer>
+  </div>
+</div>
+    `,
+
 });
 
 new Vue({
     el: '#root',
-
-    data: {
-        couponApplied: false
-    },
-
-    created(){
-        Event.$on('applied', ()=> alert('handling it'))
-    }
-
 
 });
